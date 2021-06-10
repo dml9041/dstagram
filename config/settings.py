9bd72faaae09a194ad3efb3470d281cca9bd0c93
dml9041 <dmlgus9041@naver.com>
2021-06-10 23:05:52 +0900

    heroku upload

--- config/settings.py
+++ config/settings.py
@@ -128,7 +128,9 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
